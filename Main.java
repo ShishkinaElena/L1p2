23d97c60bf7591b5fbeb6f4016d347d965858dd0
ShishkinaElena <148252907+ShishkinaElena@users.noreply.github.com>
2023-11-08 17:04:10 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package p2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Oper.sum(10, 20));
+        System.out.println(Oper.razn(20, 10));
+    }
+}
+
+class Oper{
+    static int sum(int x, int y){
+        return x+y;
+    }
+    static int razn(int x, int y){
+        return x-y;
+    }
+}
